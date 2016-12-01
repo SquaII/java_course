defbdb7229d27f5c06d342c8cf986174dad23bbc
squaii <sergey.dykin.1991@gmail.com>
2016-12-01 22:52:36 +0300

    Added configs for running testng suites.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -5,13 +5,13 @@ import com.google.gson.reflect.TypeToken;
 import com.thoughtworks.xstream.XStream;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
-import ru.stqa.pft.addressbook.model.*;
+import ru.stqa.pft.addressbook.model.ContactData;
+import ru.stqa.pft.addressbook.model.Contacts;
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -40,7 +40,6 @@ public class ContactCreationTests extends TestBase {
     @DataProvider
     public Iterator<Object[]> validContactsFromJSON() throws IOException {
         try (BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.json")))) {
-
             String json = "";
             String line = reader.readLine();
             while (line != null) {
@@ -56,6 +55,7 @@ public class ContactCreationTests extends TestBase {
 
     @Test(dataProvider = "validContactsFromXML")
     public void testContactCreation(ContactData contact) {
+        app.goTo().homePage();
         Contacts before = app.contact().all();
         app.contact().create(contact);
         Contacts after = app.contact().all();
