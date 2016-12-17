7280a0cb8bd32082c2e4557ad804331340504ab4
squaii <sergey.dykin.1991@gmail.com>
2016-12-17 15:31:38 +0300

    Added new module for mantis tests.

---
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/TestBase.java
@@ -0,0 +1,34 @@
+package ru.stqa.pft.mantis.tests;
+
+import org.openqa.selenium.remote.BrowserType;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import ru.stqa.pft.mantis.appmanager.ApplicationManager;
+
+
+public class TestBase {
+    //Logger logger = LoggerFactory.getLogger(MyClass.class);
+    protected static final ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
+
+    @BeforeSuite
+    public void setUp() throws Exception {
+        app.init();
+    }
+
+    @AfterSuite(alwaysRun = true)
+    public void tearDown() {
+        app.stop();
+    }
+
+    /*
+    @BeforeMethod
+    public void logTestStart(Method m, Object[] p) {
+        logger.info("Start test " + m.getName() + " with parameters " + Arrays.asList(p));
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void logTestStop(Method m) {
+        logger.info("Stop test " + m.getName());
+    }
+    */
+}
